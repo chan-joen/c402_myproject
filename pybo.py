da4c70476faa41f63041f49cf5465862936d8375
chan-joen <jsp9650js64@gmail.com>
2023-11-03 12:07:07 +0900

    commit

--- pybo.py
+++ pybo.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import test
+
 app = Flask(__name__)
+
+comparison_str = test.comparison.to_string()
 
 @app.route('/')
 def hello_pybo():
-    return '<h1>Hello, Pybo!</h1>'
+    #return 'Hello, Pybo! local'
+    return comparison_str
